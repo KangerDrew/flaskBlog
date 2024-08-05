6790d0f3c1bb2e5a2b3024334e8341d6229ccf32
Drew Kang <kangdrew95@gmail.com>
2024-08-04 19:33:32 -0700

    mock database is now working

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, session, redirect, url_for
+from mock import mock_database_connection
 
 load_dotenv()
 
 app = Flask(__name__)
-# url = os.getenv("DATABASE_URL")
-# connection = psycopg2.connect(url)
 
-CREATE_BLOG = (
-    "INSERT INTO blogs"
-)
+USE_MOCK_DB = os.getenv("USE_MOCK_DB", 'False') == 'True'
+
+if USE_MOCK_DB:
+    # Use the mock database connection
+    conn = mock_database_connection()
+    print("Using mocked database connection.")
+else:
+    url = os.getenv("DATABASE_URL")
+    conn = psycopg2.connect(url)
 
 
-@app.get("/")
+@app.route("/")
 def home():
-    return "<h1>My VERY COOL Blog :D</h1>"
+    # First need to check if user is logged in
+    if "loggedin" in session:
+        return "You are in my dude!"
+
+    # Otherwise, re-direct them to the login page:
+    return redirect(url_for('login'))
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return "Login Here"
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the account exists in PostgreSQL:
+
+
+    return "Login beawhwe"
 
 
 if __name__ == "__main__":
