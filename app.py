de247fa3b6ba1714ac73bc37644741798ce2dbc4
Drew Kang <kangdrew95@gmail.com>
2024-08-09 19:39:50 -0700

    need to implement password hashing and insert into database

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import psycopg2
+import psycopg2.extras
 from dotenv import load_dotenv
 from flask import Flask, request, session, redirect, url_for
 from mock import mock_database_connection
+from flask_bcrypt import Bcrypt
 
 load_dotenv()
 
@@ -32,14 +34,25 @@ def home():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
 
         # Check if the account exists in PostgreSQL:
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
 
+        if account:
+            password_rs = account['password']
+        else:
 
-    return "Login beawhwe"
+            return "<h1>You're asking for teh wrong guy</h1>"
+
+    # Load login template here
+    return "<h1>Who the fak are you</h1>"
 
 
 if __name__ == "__main__":
