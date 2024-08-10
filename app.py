6e9fd45da065b38fc02c00e00de1b5b116e98b69
Drew Kang <kangdrew95@gmail.com>
2024-08-09 19:47:09 -0700

    basic authentication method implemented

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ from flask import Flask, request, session, redirect, url_for
 from mock import mock_database_connection
 from flask_bcrypt import Bcrypt
 
+# Currently following tutorial from this:
+# https://tutorial101.blogspot.com/2021/04/python-flask-postgresql-login-register.html
+# https://www.youtube.com/watch?v=xIiNYn0q1gs
+
 load_dotenv()
 
 app = Flask(__name__)
+bcrypt = Bcrypt()
 
 USE_MOCK_DB = os.getenv("USE_MOCK_DB", 'False') == 'True'
 
@@ -25,7 +30,7 @@ else:
 @app.route("/")
 def home():
     # First need to check if user is logged in
-    if "loggedin" in session:
+    if session["loggedin"]:
         return "You are in my dude!"
 
     # Otherwise, re-direct them to the login page:
@@ -40,15 +45,23 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
+        password_hashed = bcrypt.generate_password_hash(password).decode('utf-8')
+
         # Check if the account exists in PostgreSQL:
-        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password_hashed))
         # Fetch one record and return result
         account = cursor.fetchone()
 
+        # Account exists, so re-direct to home page:
         if account:
-            password_rs = account['password']
-        else:
+            session['loggedin'] = True
+            session['username'] = account['username']
 
+            # Redirect to home page:
+            return redirect(url_for('home'))
+
+        # Account not found, tell them to fk off:
+        else:
             return "<h1>You're asking for teh wrong guy</h1>"
 
     # Load login template here
